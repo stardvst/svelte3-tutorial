9c6c6990d153a97e67deff47d8f64122bde8099e
stardvst <lilia.manukyan@hotmail.com>
2025-04-27 19:49:12 +0400

    add view and add form components

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,5 @@
 <script>
-  import List from "./components/List.svelte";
-  import Obj from "./components/Obj.svelte";
-
-  const content =
-    `# Markdown here!` +
-    `\nHere's a **bold** text and a [link](https://example.com).`;
+  import ViewAdd from "./components/ViewAdd.svelte";
 </script>
 
-<List date={Date.now() / 1000} />
-<Obj objectId="00000000-0000-0000-0000-000000000000" />
+<ViewAdd />
